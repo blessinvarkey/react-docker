b94d93022a135fa801fb1b48439793d217407ca0
blessin.varkey <blessinvarkey@gmail.com>
2022-02-27 23:59:02 +0530

    update Dockerfile, Index.jsx

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
+# pull the official base image
 FROM node 
+# set working directory
 WORKDIR /app
+
 COPY package.json .
 RUN npm install
+# add app
 COPY . .
 EXPOSE 3000
 CMD ["npm", "start"]
---
+++ src/components/index/Index.jsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Index() {
+  return (
+      <div> This is a text file</div>
+  )
+}
+
+const styles = StyleSheet.create({})
